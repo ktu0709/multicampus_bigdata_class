c6deb5997fc737eb9b661e0aad0a54e2a6d2d071
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2023-11-10 16:17:37 +0900

    Delete C_set.py

--- C_set.py
+++
@@ -1,9 +0,0 @@
-a={1,4,5,6,10}
-b={6,10,7,8,9}
-print(a)
-print(b)
-
-print('=======합집합 :',(a|b), a.union(b))
-print('=======차집합 :',(a-b), a.difference(b))
-print('=======교집합 :',(a&b), a.intersection(b))
-print('=======배타 집합 :',(a^b), a.symmetric_difference(b))
