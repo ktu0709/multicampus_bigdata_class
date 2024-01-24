91568c0aa49ad0dc6c911fa8bd94301e5e41bed1
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2024-01-24 23:46:45 +0900

    Create create_apiTB.sql

---
+++ semi_pjt/create_apiTB.sql
@@ -0,0 +1,33 @@
+use semi_pjt;
+
+create table api_15074422(
+resultCode int,
+resultMsg  varchar(50),
+instiNm varchar(100),
+rgnCd  varchar(10),
+rgnNm  varchar(100),
+brchCd varchar(10),
+brchNm varchar(10),
+totCoseCnt int,
+totTraineeCnt int,
+totQualAcquCnt int,
+coseQualgbCd  varchar(5),
+coseQualgbNm  varchar(100),
+coseId varchar(100),
+coseSeq int,
+coseNm  varchar(100),
+coseInstiGbCd varchar(10),
+coseInstiGbNm  varchar(100),
+coseOprTypCd varchar(50),
+coseOprTypNm varchar(100),
+coseTrngStartDt varchar(50),
+coseTrngEndDt varchar(50),
+coseJmCd  varchar(10),
+coseJmNm  varchar(100),
+coseOrganStdVerCd varchar(50),
+coseTraineeCnt int,
+coseQualAcquCnt int,
+numOfRows int,
+pageNo  int,
+totalCount int
+)
